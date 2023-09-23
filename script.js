95b665b3a298510a4dad026720020b30b3ea9d45
Sudhakar <sidhu1171bansal@gmail.com>
2023-09-23 11:19:34 +0530

    Displaying books

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const thead = document.body.getElementsByTagName('thead');
+import storedBooks from "./storedBooks.js";
+
+//display the stored books
+
+function displayBooks() {
+    const books = storedBooks;
+    books.forEach((book) => {
+        addBooks(book);
+    });
+}
+document.addEventListener("DOMContentLoaded", displayBooks);
+
+
+//Adding the records
+
+function addBooks(book) {
+    const booklist = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${book.isbn}</td>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td><button class="delbtn">&#10062;</button></td>`;
+    booklist.appendChild(row);
+}
+
+
 const submitButton = document.querySelector('#submit');
-
-
 submitButton.addEventListener('click', ((e) => {
     e.preventDefault();
     const title = document.querySelector('#title');
@@ -9,16 +32,16 @@ submitButton.addEventListener('click', ((e) => {
     const ISBN = document.querySelector('#isbn');
     addBooks(ISBN.value, title.value, author.value);
     clearForm(ISBN, title, author);
-
 }))
 
-function addBooks(isbn, title, author) {
-    const booklist = document.querySelector('#book-list');
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${isbn}</td><td>${title}</td><td>${author}</td>`
-    booklist.appendChild(row);
-}
+//deleting the records
+const delbtn = document.getElementsByTagName('button')[0];
+delbtn.addEventListener('click', ((e) => {
+    console.log(delbtn);
+}))
 
+
+//clearing the form
 function clearForm(isbn, title, author) {
     title.value = "";
     author.value = "";
---
+++ storedBooks.js
@@ -0,0 +1,14 @@
+const storedBooks = [
+    {
+        isbn: "1223",
+        title: "Book1",
+        author: "Author1 Book1"
+    },
+    {
+        isbn: "1224",
+        title: "Book2",
+        author: "Author2 Book2"
+    }
+];
+
+export default storedBooks;
